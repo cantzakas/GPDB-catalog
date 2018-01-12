a898097b6974469689cefaf78bde259b7d770f0c
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-12 15:59:42 +0000

    Update psql_describe_table.sql

--- table-info/psql_describe_table.sql
+++ table-info/psql_describe_table.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE FUNCTION gpdb_toolkit.psql_describe_table(p_db_name text, p_sc
 RETURNS text
 AS $$
     import os
-    p = os.popen('psql -U ' + p_db_name + ' -c "\d+ '+ p_sch_name + '.' + p_tbl_name + '"')
+    p = os.popen('psql -U ' + p_db_name + ' -c "\d '+ p_sch_name + '.' + p_tbl_name + '"')
     return p.read() $$ 
 LANGUAGE plpythonu;
