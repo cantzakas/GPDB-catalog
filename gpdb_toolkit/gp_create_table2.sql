8475a136f887a2bf36500381387f294cbc6f2dcd
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-12 16:56:19 +0000

    Update gp_create_table2.sql

--- gpdb_toolkit/gp_create_table2.sql
+++ gpdb_toolkit/gp_create_table2.sql
@@ -4,9 +4,9 @@ AS $$
     import os
     import re
 
-    out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname | grep  -vE '^SET')
+    out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname + ' | grep  -vE \'^SET\'')
     out_tables = re.findall(r'^(?:--\n-- Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^\s]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )([^\s]*)(?:\n--\n)([^-]+-{1}[^-]+|[^-]+)', out_pg_dump.read(), flags = re.MULTILINE)
     out_result = [v[5] for i, v in enumerate(out_tables) if v[2] + '.' + v[0] ==  in_schema + '.' + in_tblname]
 
-    return out_result[0] $$ 
+    return 'SET search_path = ' + in_schema + ';\n\n' + out_result[0] $$ 
 LANGUAGE plpythonu;
