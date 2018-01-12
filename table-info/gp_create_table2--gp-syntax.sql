353098387594e1a3d42f9f4e6578b64f06581aa0
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-12 13:09:34 +0000

    Create gp_create_table2--gp-syntax.sql
    Get table definition (GPDB style) using *pg_dump -c -s --gp-syntax  + <database>* command

---
+++ table-info/gp_create_table2--gp-syntax.sql
@@ -0,0 +1,14 @@
+```sql
+CREATE OR REPLACE FUNCTION gpdb_toolkit.gp_create_table2(in_dbname text, in_schema text, in_tblname text) 
+RETURNS text
+AS $$
+    import os
+    import re
+
+    out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname | grep  -vE '^SET')
+    out_tables = re.findall(r'^(?:--\n-- Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^\s]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )([^\s]*)(?:\n--\n)([^-]+-{1}[^-]+|[^-]+)', out_pg_dump.read(), flags = re.MULTILINE)
+    out_result = [v[5] for i, v in enumerate(out_tables) if v[2] + '.' + v[0] ==  in_schema + '.' + in_tblname]
+
+    return out_result[0] $$ 
+LANGUAGE plpythonu;
+```
