67ac4870650dd707aeb4924757f03604449a6598
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-15 12:45:40 +0000

    UDF to cover UNIQUE & PRIMARY KEY constraints

--- gpdb_toolkit/gp_create_table2.sql
+++ gpdb_toolkit/gp_create_table2.sql
@@ -6,7 +6,10 @@ AS $$
 
     out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname + ' | grep  -vE \'^SET\'')
     out_tables = re.findall(r'^(?:--\n-- Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^\s]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )([^\s]*)(?:\n--\n)([^-]+-{1}[^-]+|[^-]+)', out_pg_dump.read(), flags = re.MULTILINE)
-    out_result = [v[5] for i, v in enumerate(out_tables) if v[2] + '.' + v[0] ==  in_schema + '.' + in_tblname]
+    
+    table_pattern = re.compile(r'^' + in_tblname + '(_pkey|\w+_key)*$')
+    
+    out_result = [v[5] for i, v in enumerate(out_tables) if table_pattern.match(v[2]) and v[0] == in_schema]
 
-    return 'SET search_path = ' + in_schema + ';\n\n' + out_result[0] $$ 
+    return 'SET search_path = gptika;' + '\n'.join(out_result) $$ 
 LANGUAGE plpythonu;
