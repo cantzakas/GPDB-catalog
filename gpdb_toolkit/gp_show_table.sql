07d481d01cf74f0cc96f7890f96dda4aa388dba7
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-16 15:40:26 +0000

    Update UDF to include schema in pg_dump call

--- gpdb_toolkit/gp_show_table.sql
+++ gpdb_toolkit/gp_show_table.sql
@@ -4,7 +4,7 @@ AS $$
     import os
     import re
 
-    out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname + ' | grep  -vE \'^SET\'')
+    out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax --schema=' + in_schema + ' ' + in_dbname + ' | grep  -vE \'^SET\'')
     out_tables = re.findall(r'^(?:--\n-- Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^\s]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )([^\s]*)(?:\n--\n)([^-]+-{1}[^-]+|[^-]+)', out_pg_dump.read(), flags = re.MULTILINE)
     
     table_pattern = re.compile(r'^.+\b' + in_tblname + r'{1}\b.+$', re.DOTALL)
