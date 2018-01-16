c94eae59d0192039f59e99dab7b294d13b1979f8
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-16 14:20:44 +0000

    Create gp_show_table.sql

---
+++ gpdb_toolkit/gp_show_table.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION gpdb_toolkit.gp_show_table(in_dbname text, in_schema text, in_tblname text) 
+RETURNS text
+AS $$
+    import os
+    import re
+
+    out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname + ' | grep  -vE \'^SET\'')
+    out_tables = re.findall(r'^(?:--\n-- Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^\s]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )([^\s]*)(?:\n--\n)([^-]+-{1}[^-]+|[^-]+)', out_pg_dump.read(), flags = re.MULTILINE)
+    
+    table_pattern = re.compile(r'^.+\b' + in_tblname + r'{1}\b.+$', re.DOTALL)
+    
+    out_results = [v[5] for i, v in enumerate(out_tables) if v[2] == in_schema and table_pattern.match(v[5])]
+
+    return 'SET search_path = gptika;\n' + '\n'.join(out_results) $$ 
+LANGUAGE plpythonu;
