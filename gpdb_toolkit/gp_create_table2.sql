4b2ff0783dc6fb4abcad635fb4733903062bcff5
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-16 12:33:48 +0000

    UDF to cover INDEX on table edge case

--- gpdb_toolkit/gp_create_table2.sql
+++ gpdb_toolkit/gp_create_table2.sql
@@ -7,9 +7,9 @@ AS $$
     out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax ' + in_dbname + ' | grep  -vE \'^SET\'')
     out_tables = re.findall(r'^(?:--\n-- Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^\s]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )([^\s]*)(?:\n--\n)([^-]+-{1}[^-]+|[^-]+)', out_pg_dump.read(), flags = re.MULTILINE)
     
-    table_pattern = re.compile(r'^' + in_tblname + '(_pkey|\w+_key)*$')
+    table_pattern = re.compile(r'^.+\b' + in_tblname + r'{1}\b.+$', re.DOTALL)
     
-    out_result = [v[5] for i, v in enumerate(out_tables) if table_pattern.match(v[2]) and v[0] == in_schema]
+    out_results = [v[5] for i, v in enumerate(out_tables) if v[2] == in_schema and table_pattern.match(v[5])]
 
-    return 'SET search_path = gptika;' + '\n'.join(out_result) $$ 
+    return 'SET search_path = gptika;' + '\n'.join(out_results) $$ 
 LANGUAGE plpythonu;
