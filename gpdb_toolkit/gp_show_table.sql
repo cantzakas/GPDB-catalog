0aaac4862d27a3e8d71d8a865400f7c7d7a6ad8f
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2018-01-19 13:59:16 +0000

    Update gp_show_table.sql

--- gpdb_toolkit/gp_show_table.sql
+++ gpdb_toolkit/gp_show_table.sql
@@ -5,10 +5,10 @@ AS $$
     import re
 
     out_pg_dump = os.popen('/usr/local/greenplum-db/bin/pg_dump -U ' + in_dbname + ' -c -s --gp-syntax --schema=' + in_schema + ' ' + in_dbname + ' | sed -e \'s/^--/§/\' | grep -vE \'^SET\'')
-    out_tables = re.findall(r'^(?:§ Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^;]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )?([^\s]*)?\n*(?:^§$)(?:\n+)([^§]*)(?:^§$)', out_pg_dump.read(), flags = re.MULTILINE)
+    out_db = re.findall(r'^(?:§ Name: )([^;]*)(?:; Type: )([^;]*)(?:; Schema: )([^;]*)(?:; Owner: )([^\s]*)(?:; Tablespace: )?([^\s]*)?\n*(?:^§$)(?:\n+)([^§]*)(?:^§$)', out_pg_dump.read(), flags = re.MULTILINE)
     
     table_pattern = re.compile(r'^.+\b' + in_tblname + r'{1}\b.+$', re.DOTALL)
-    out_results = [v[5] for i, v in enumerate(out_tables) if v[2] == in_schema and table_pattern.match(v[5])]
+    out_results = [v[5] for i, v in enumerate(out_db) if v[2] == in_schema and table_pattern.match(v[5])]
 
     return 'SET search_path = ' + in_schema + ';\n' + '\n'.join(out_results) 
 $$ LANGUAGE plpythonu;
