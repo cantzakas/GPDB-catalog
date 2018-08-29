428712fe6d477ca50b8ce4cae60ea87118e61727
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2018-08-29 12:22:36 +0100

    Update psql_generate_column_list.sql

--- gpdb_toolkit/psql_generate_column_list.sql
+++ gpdb_toolkit/psql_generate_column_list.sql
@@ -1,13 +1,13 @@
 DROP FUNCTION public.generate_column_list(VARCHAR, VARCHAR, VARCHAR);
 
-CREATE OR REPLACE FUNCTION public.generate_column_list(in_nmspace VARCHAR, in_class VARCHAR, in_name VARCHAR) 
+CREATE OR REPLACE FUNCTION public.generate_column_list(in_nmspace VARCHAR, in_class VARCHAR, in_prefix_name VARCHAR) 
 RETURNS TEXT AS $$
 DECLARE
 	res TEXT;
 BEGIN
 	SELECT string_agg(new_column_name, ', ') INTO res
 	FROM (
-		SELECT pg_attribute.attname::TEXT || ' AS ' || generate_col_list.in_name || '_' || pg_attribute.attname::TEXT AS new_column_name 
+		SELECT pg_attribute.attname::TEXT || ' AS ' || generate_col_list.in_prefix_name || '_' || pg_attribute.attname::TEXT AS new_column_name 
 		FROM pg_attribute, pg_class, pg_namespace
 		WHERE pg_attribute.attrelid = pg_class.oid
 		AND pg_class.relnamespace = pg_namespace.oid
